ecab9a79594e68b86fcda2aba6fdddc090ba2914
Georgii Sirotkin <georgiysirotkin@gmail.com>
2016-02-11 23:53:13 +0300

    added auth, routing and controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,7 +9,7 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,8 +24,30 @@ Route::get('/', function () {
 | it contains. The "web" middleware group is defined in your HTTP
 | kernel and includes session state, CSRF protection, and more.
 |
-*/
+ */
 
-Route::group(['middleware' => ['web']], function () {
-    //
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
+
+    Route::get('words/random', ['as' => 'random_word', 'uses' => 'WordsController@randomWord']);
+    Route::resource('/words', 'WordsController', ['names' => [
+        'index' => 'words',
+        'create' => 'add_word',
+        'store' => 'insert_word',
+        'show' => 'view_word',
+        'edit' => 'edit_word',
+        'update' => 'update_word',
+        'destroy' => 'delete_word',
+    ]]);
+
+    // main (statistics)+
+    // random+
+    // find
+    // add+
+    // edit+
+    // words+
+    // delete+
+    // view+
 });
